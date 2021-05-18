c3c1e0de81421a901a2e0a889fa3d6bded3a166d
AdrianAdame <47167208+AdrianAdame@users.noreply.github.com>
2021-05-17 22:33:15 -0700

    Add files via upload

---
+++ Java/Practica 9/Principal.java
@@ -0,0 +1,103 @@
+import IO.ClaseIO;
+
+import java.io.*;
+import java.util.Random;
+
+public class Principal {
+    public static void main(String [] args){
+        Random random = new Random();
+
+        String[] calificacionesArchivo;
+        Alumno[] alumnos = new Alumno[3];
+
+        for(int i =0; i < alumnos.length; i++){
+            alumnos[i] = new Alumno(
+                    ClaseIO.capturaCadena("Nombre del alumno [" + (i+1) + "]"),
+                    ClaseIO.capturaCadena("Matricula"),
+                    7
+            );
+        }
+
+        calificacionesArchivo=extraerDatosArchivo("practica9.csv",',');
+
+        for (Alumno value : alumnos) {
+            float[] calificaciones = new float[7];
+            for (int j = 0; j < calificaciones.length; j++) {
+                try {
+                    calificaciones[j] = Float.parseFloat(
+                            calificacionesArchivo[random.nextInt(calificacionesArchivo.length)]
+                    );
+                } catch (NumberFormatException e) {
+                    ClaseIO.imprimirMensaje("ERROR: Formateo de valor no valido!");
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                    ClaseIO.imprimirMensaje("ERROR: Indice fuera de rango");
+                    ex.printStackTrace();
+                }
+            }
+
+            value.setCalificaciones(calificaciones);
+        }
+
+        for(Alumno alumno : alumnos){
+            alumno.mostrarAlumno();
+            ClaseIO.pausa();
+            ClaseIO.imprimirMensaje("\n");
+        }
+
+        guardardatos("alumnos", alumnos);
+    }
+
+    private static String[] extraerDatosArchivo(String documento, char separador){
+
+        FileInputStream fileInputStream = null;
+        int nBytes;
+        byte[] buffer = new byte[81];
+        String texto = "";
+        String[] calificacionesArchivo;
+
+        try{
+            fileInputStream = new FileInputStream(documento);
+            nBytes = fileInputStream.read(buffer, 0, 81);
+            texto = new String(buffer, 0, nBytes);
+        }catch (FileNotFoundException e) {
+            ClaseIO.imprimirMensaje("ERROR: Documento no encontrado!");
+        }catch (IOException ex){
+            ClaseIO.imprimirMensaje("ERROR: Error en el flujo de archivo!");
+        }finally {
+            if(fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    ClaseIO.imprimirMensaje("ERROR: Flujo de documento no se pudo cerrar correctamente!");
+                }
+            }
+        }
+
+        calificacionesArchivo = texto.split(String.valueOf(separador));
+
+        return calificacionesArchivo;
+    }
+
+    private static void guardardatos(String nombreSalidaDocumento, Object[] objetoAGuardar){
+        FileOutputStream archivoSalida = null;
+
+        try{
+            archivoSalida = new FileOutputStream(nombreSalidaDocumento + ".txt");
+            for(Object objeto: objetoAGuardar){
+                if( objeto instanceof Alumno)
+                archivoSalida.write(((Alumno) objeto).guardarAlumno());
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(archivoSalida != null) {
+                try {
+                    archivoSalida.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                ClaseIO.imprimirMensaje("Archivo de datos generado!");
+            }
+        }
+    }
+}
